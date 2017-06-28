e662fe90d14efd6e112a7d4adb6a1ae45afc7a65
Jay <isaacsdaddy42293@gmail.com>
2017-06-28 16:42:50 -0400

    completed

--- headache/Program.cs
+++ headache/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,43 @@ namespace headache
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("How old are you?");
+            int ageOfPerson = int.Parse(Console.ReadLine());
+            if (ageOfPerson >= 0 && ageOfPerson <= 2)
+            {
+                Console.WriteLine("Still in mama's arms");
+            }
+            else if (ageOfPerson >= 3 && ageOfPerson <= 4)
+            {
+                Console.WriteLine("Preschool Maniac");
+            }
+            else if (ageOfPerson >= 5 && ageOfPerson <= 11)
+            {
+                Console.WriteLine("Elementery School");
+            }
+            else if (ageOfPerson >= 12 && ageOfPerson <= 14)
+            {
+                Console.WriteLine("Middle School");
+            }
+            else if (ageOfPerson >= 15 && ageOfPerson <= 18)
+            {
+                Console.WriteLine("High School");
+            }
+            else if (ageOfPerson >= 19 && ageOfPerson <= 22)
+            {
+                Console.WriteLine("College");
+            }
+            else if (ageOfPerson >= 23 && ageOfPerson <= 65)
+            {
+                Console.WriteLine("Fucked");
+            }
+            else if (ageOfPerson >= 66 && ageOfPerson <= 100)
+            {
+                Console.WriteLine("Dead from drinking to much");
+            }
+            else Console.WriteLine("This is for HuMaNs!!!");
+
+        }
         }
     }
-}
+
